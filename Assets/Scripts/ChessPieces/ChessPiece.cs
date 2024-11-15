f0a2518542f5638d667428b690ca32f6f61b0047
khkim09 <09ricky@naver.com>
2024-11-15 17:34:44 +0900

    add pawn simple moving & killing move

--- Assets/Scripts/ChessPieces/ChessPiece.cs
+++ Assets/Scripts/ChessPieces/ChessPiece.cs
@@ -31,7 +31,7 @@ public class ChessPiece : MonoBehaviour
         transform.localScale = Vector3.Lerp(transform.localScale, desiredScale, Time.deltaTime * 10);
     }
 
-    public List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    public virtual List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
     {
         List<Vector2Int> l = new List<Vector2Int>();
 
--- Assets/Scripts/ChessPieces/Pawn.cs
+++ Assets/Scripts/ChessPieces/Pawn.cs
@@ -3,4 +3,42 @@ using UnityEngine;
 
 public class Pawn : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> l = new List<Vector2Int>();
+
+        int direction = (team == 0) ? 1 : -1; // white : move up (1), black : move down (-1)
+
+        // pawn simple move
+        if (team == 0) // white pawn의 첫 움직임 (1, 2칸 전진 중 선택)
+        {
+            if (board[currentX, currentY + direction] == null)
+                l.Add(new Vector2Int(currentX, currentY + direction));
+
+            if (currentY == 1)
+                if (board[currentX, currentY + direction * 2] == null)
+                    if (board[currentX, currentY + direction] == null)
+                        l.Add(new Vector2Int(currentX, currentY + direction * 2));
+        }
+        else // black pawn 동일
+        {
+            if (board[currentX, currentY + direction] == null)
+                l.Add(new Vector2Int(currentX, currentY + direction));
+
+            if (currentY == 6)
+                if (board[currentX, currentY + direction * 2] == null)
+                    if (board[currentX, currentY + direction] == null)
+                        l.Add(new Vector2Int(currentX, currentY + direction * 2));
+        }
+
+        // pawn killing move
+        if (board[currentX + 1, currentY + direction] != null) // 우측 앞에 chess 말 존재
+            if (board[currentX, currentY].team != board[currentX + 1, currentY + direction].team) // 해당 chess 말이 상대 team일 경우
+                l.Add(new Vector2Int(currentX + 1, currentY + direction));
+        if (board[currentX - 1, currentY + direction] != null)
+            if (board[currentX, currentY].team != board[currentX - 1, currentY + direction].team)
+                l.Add(new Vector2Int(currentX - 1, currentY + direction));
+
+        return l;
+    }
 }
