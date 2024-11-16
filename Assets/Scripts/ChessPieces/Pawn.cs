5267ae2c73ad2a7c7d194eaa6372daa73c135252
khkim09 <09ricky@naver.com>
2024-11-16 20:52:00 +0900

    modify pawn move's error

--- Assets/Scripts/ChessPieces/Pawn.cs
+++ Assets/Scripts/ChessPieces/Pawn.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Pawn : ChessPiece
 {
@@ -12,7 +13,8 @@ public class Pawn : ChessPiece
         // pawn simple move
         if (team == 0) // white pawn의 첫 움직임 (1, 2칸 전진 중 선택)
         {
-            if (currentX == 0) // pawn이 젤 끝 (전진 할 곳 없는 위치)에 도착시 null error 발생 - 수정 필요
+            if (currentY == 7) // change piece animation
+                return l;
 
             if (board[currentX, currentY + direction] == null)
                 l.Add(new Vector2Int(currentX, currentY + direction));
@@ -24,6 +26,9 @@ public class Pawn : ChessPiece
         }
         else // black pawn 동일
         {
+            if (currentY == 0) // change piece animation
+                return l;
+
             if (board[currentX, currentY + direction] == null)
                 l.Add(new Vector2Int(currentX, currentY + direction));
 
@@ -34,13 +39,27 @@ public class Pawn : ChessPiece
         }
         
         // pawn killing move
-        // 좌우 끝에 있는 pawn의 kill가능 여부 파악에서 error - 수정 필요
-        if (board[currentX + 1, currentY + direction] != null) // 우측 앞에 chess 말 존재
-            if (board[currentX, currentY].team != board[currentX + 1, currentY + direction].team) // 해당 chess 말이 상대 team일 경우
-                l.Add(new Vector2Int(currentX + 1, currentY + direction));
-        if (board[currentX - 1, currentY + direction] != null) // 좌측 동일 작업
-            if (board[currentX, currentY].team != board[currentX - 1, currentY + direction].team)
-                l.Add(new Vector2Int(currentX - 1, currentY + direction));
+        if (currentX == 0) // 좌측 끝 열 - 오른쪽만 kill 가능 (index out of range exception 방지)
+        {
+            if (board[currentX + 1, currentY + direction] != null)
+                if (board[currentX, currentY].team != board[currentX + 1, currentY + direction].team)
+                    l.Add(new Vector2Int(currentX + 1, currentY + direction));
+        }
+        else if (currentX == 7) // 우측 끝 열 - 왼쪽만 kill 가능
+        {
+            if (board[currentX - 1, currentY + direction] != null)
+                if (board[currentX, currentY].team != board[currentX - 1, currentY + direction].team)
+                    l.Add(new Vector2Int(currentX - 1, currentY + direction));
+        }
+        else
+        {
+            if (board[currentX + 1, currentY + direction] != null) // 우측 앞에 chess 말 존재
+                if (board[currentX, currentY].team != board[currentX + 1, currentY + direction].team) // 해당 chess 말이 상대 team일 경우
+                    l.Add(new Vector2Int(currentX + 1, currentY + direction));
+            if (board[currentX - 1, currentY + direction] != null) // 좌측 동일 작업
+                if (board[currentX, currentY].team != board[currentX - 1, currentY + direction].team)
+                    l.Add(new Vector2Int(currentX - 1, currentY + direction));
+        }
         
         return l;
     }
