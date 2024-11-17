5e8b114eb978b28e30d5bcff5fe3fb6aed95d03b
khkim09 <09ricky@naver.com>
2024-11-17 23:31:34 +0900

    modify bishop move (separate case)

--- Assets/Scripts/ChessPieces/Bishop.cs
+++ Assets/Scripts/ChessPieces/Bishop.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class Bishop : ChessPiece
@@ -7,58 +8,58 @@ public class Bishop : ChessPiece
     {
         List<Vector2Int> l = new List<Vector2Int>();
 
-        // left diagonal - 합 일정 & 각 좌표 chessboard 내부로
-        int sum = currentX + currentY;
-        for (int i = 0; i < tileCountX; i++)
-            for (int j = 0; j < tileCountY; j++)
-                if (i + j == sum)
-                {
-                    if (board[i, j] == null)
-                        l.Add(new Vector2Int(i, j));
-                    else
-                        if (board[i, j].team != board[currentX, currentY].team)
-                            l.Add(new Vector2Int(i, j));
-                }
-
-        /*
-        // 수정 필요
-        // right diagonal - 좌표 차이 유지
-        int diff = currentX - currentY;
-        for (int i = 0; i < tileCountX; i++)
-            for (int j = 0; j < tileCountY; j++)
-                if (board[i, i + diff] == null)
-                    
-        */
-
-        // 아래는 버려
-        /*
-        for (int i = 0; i < tileCountX; i++)
-            for (int j = 0; j < tileCountY; j++)
+        // left diagonal
+        for (int i = currentX - 1, j = currentY + 1; currentX >= 0 && currentY < tileCountY; i--, j++) // 좌상향
+        {
+            if (board[i, j] == null)
+                l.Add(new Vector2Int(i, j));
+            else
             {
-                if (i < j)
-                {
-                    if (j - i == diff)
-                    {
-                        if (board[i, j] == null)
-                            l.Add(new Vector2Int(i, j));
-                        else
-                            if (board[i, j].team != board[currentX, currentY].team)
-                                l.Add(new Vector2Int(i, j));
-                    }
-                }
-                else
-                {
-                    if (i - j == diff)
-                    {
-                        if (board[i, j] == null)
-                            l.Add(new Vector2Int(i, j));
-                        else
-                            if (board[i, j].team != board[currentX, currentY].team)
-                                l.Add(new Vector2Int(i, j));
-                    }
-                }
+                if (board[i, j].team != board[currentX, currentY].team) // 다른 team (kill)
+                    l.Add(new Vector2Int(i, j));
+                else // 같은 team - break (그 이후로 같은 방향 이동 불가)
+                    break;
             }
-        */
+        }
+        for (int i = currentX + 1, j = currentY - 1; currentX < tileCountX && currentY >= 0; i++, j--) // 우하향
+        {
+            if (board[i, j] == null)
+                l.Add(new Vector2Int(i, j));
+            else
+            {
+                if (board[i, j].team != board[currentX, currentY].team)
+                    l.Add(new Vector2Int(i, j));
+                else
+                    break;
+            }
+        }
+
+        // right diagonal
+        for (int i = currentX + 1, j = currentY + 1; currentX < tileCountX && currentY< tileCountY; i++, j++) // 우상향
+        {
+            if (board[i, j] == null)
+                l.Add(new Vector2Int(i, j));
+            else
+            {
+                if (board[i, j].team != board[currentX, currentY].team)
+                    l.Add(new Vector2Int(i, j));
+                else
+                    break;
+            }
+        }
+        for (int i = currentX - 1, j = currentY - 1; currentX >= 0 && currentY >= 0; i--, j--) // 좌하향
+        {
+            if (board[i, j] == null)
+                l.Add(new Vector2Int(i, j));
+            else
+            {
+                if (board[i, j].team != board[currentX, currentY].team)
+                    l.Add(new Vector2Int(i, j));
+                else
+                    break;
+            }
+        }
+
         return l;
     }
 }
