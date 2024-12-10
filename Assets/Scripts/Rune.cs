27bf190199c23b4b766cfc49f6e3220d3d58e793
Cheshire75 <jhr75468@gmail.com>
2024-12-11 02:18:57 +0900

    주석 정리, 제자리 점멸 오류 수정

--- Assets/Scripts/Rune.cs
+++ Assets/Scripts/Rune.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
+//타일에 할당된 문양의 이름을 표시하기 위한 클래스
 public class Rune : MonoBehaviour
 {
     public string tileRune = "None";
